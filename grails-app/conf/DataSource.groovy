43b7c39423211b5629caa4bade409dc1229637d1
--global <naden.franciscus@sensis.com.au>
2014-04-30 15:07:01 +1000

    Updated project to Grails 2.2.4 and Cassandra-Thrift 1.2.16

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,34 +1,20 @@
 dataSource {
-	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
-	username = "sa"
-	password = ""
+    pooled          = true
+    driverClassName = "org.h2.Driver"
+    username        = "sa"
+    password        = ""
+    dbCreate        = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+    url             = "jdbc:h2:mem:memDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+    maxWait         = "3000"
+    maxIdle         = "-1"
+    maxActive       = "-1"
 }
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
-// environment specific settings
+
 environments {
-	development {
-		dataSource {
-			//dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			//url = "jdbc:hsqldb:mem:devDB"
-			dbCreate = "update"
-			url = "jdbc:hsqldb:file:devDb;shutdown=true"
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
-		}
-	}
-	production {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-		}
-	}
+  // all environments currently use the default configuration
 }
